695d6d8734242294c4aff081cd524ea902b71055
Draweshwarkumar <shivamjha22948@gmail.com>
2024-10-17 14:30:36 +0530

    .

--- classroom/server.js
+++ classroom/server.js
@@ -1,51 +1,14 @@
 const express = require("express");
 const app = express();
+const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
 
 app.get("/" ,(req,res) =>{
     res.send("Hi, I am root!");
 });
 
-// GET - users
-app.get("/" , (req,res) =>{
-    res.send("GET for users ");
-})
-
-// SHOW - users
-app.get("/users/:id", (req,res) =>{
-    res.send("GET for user id");
-})
-
-// POST - users
-app.post("/users", (req,res) =>{
-    res.send("POST for users");
-})
-
-// DELETE - users
-app.delete("/users/:id", (req,res) =>{
-    res.send("DELETE for user id");
-})
-
-// posts
-// Index
-app.get("/posts" , (req,res) =>{
-    res.send("GET for posts");
-})
-
-// Show
-app.get("/posts/:id", (req,res) =>{
-    res.send("Get for post id");
-});
-
-// POST
-app.post("/posts", (req,res) =>{
-    res.send("POST for posts");
-});
-
-// DELETE
-app.delete("/posts/:id", (req,res) =>{
-    res.send("DELETE for posts id");
-})
-
+app.use("/users", users);
+app.use("/post", posts);
 
 app.listen(3000, () =>{
     console.log("Server is listening to 3000");
