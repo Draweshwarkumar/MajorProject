65e3a90ff495fed85fe4711c67abaaddb9276219
Draweshwarkumar <shivamjha22948@gmail.com>
2024-11-04 18:45:01 +0530

    adding flash message

--- classroom/server.js
+++ classroom/server.js
@@ -4,24 +4,42 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
-app.use(
-session({
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+const sessionOptions = {
     secret:"mysupersecretstring",
     resave: false,
     saveUninitialized: true,
-})
-);
+};
 
-app.get("/reqcount",(req,res) =>{
-    if(req.session.count){
-        req.session.count++;
-    }
-    else{
-        req.session.count = 1;
-    }
-    res.send(`You sent a request ${req.session.count} times`);
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.get("/register", (req,res) =>{
+    let {name = "anonymous"} = req.query;
+    req.session.name = name;
+    req.flash("success","user registered successfully");
+   res.redirect("/hello");
 });
+
+app.get("/hello",(req,res) =>{
+    res.render("page.ejs",{name: req.session.name, msg: req.flash("success")});
+});
+
+
+// app.get("/reqcount",(req,res) =>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }
+//     else{
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
 
 
 
