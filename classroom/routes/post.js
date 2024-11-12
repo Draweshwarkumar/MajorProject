c4d55b32f93b1aaa0e6b058c84d76084b4669fa8
Draweshwarkumar <shivamjha22948@gmail.com>
2024-11-12 19:18:36 +0530

    Update post.js

--- classroom/routes/post.js
+++ classroom/routes/post.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-// posts
-// Index
-// router.get("/" , (req,res) =>{
-//     res.send("GET for posts");
-// })
+posts
+Index
+router.get("/" , (req,res) =>{
+    res.send("GET for posts");
+})
 
-// // Show
-// router.get("/:id", (req,res) =>{
-//     res.send("Get for post id");
-// });
+// Show
+router.get("/:id", (req,res) =>{
+    res.send("Get for post id");
+});
 
-// // POST
-// router.post("/", (req,res) =>{
-//     res.send("POST for posts");
-// });
+// POST
+router.post("/", (req,res) =>{
+    res.send("POST for posts");
+});
 
-// // DELETE
-// router.delete("/:id", (req,res) =>{
-//     res.send("DELETE for posts id");
-// })
+// DELETE
+router.delete("/:id", (req,res) =>{
+    res.send("DELETE for posts id");
+})
 
-// module.exports=router;
+module.exports=router;
