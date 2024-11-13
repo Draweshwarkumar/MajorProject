c6c1e04de195f0a2609f19ba36ec0ffee795dd3a
Draweshwarkumar <shivamjha22948@gmail.com>
2024-11-13 21:49:48 +0530

    Update user.js

--- classroom/routes/user.js
+++ classroom/routes/user.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// GET - users
-// router.get("/" , (req,res) =>{
-//     res.send("GET for users ");
-// })
+GET - users
+router.get("/" , (req,res) =>{
+    res.send("GET for users ");
+})
 
-// // SHOW - users
-// router.get("/:id", (req,res) =>{
-//     res.send("GET for user id");
-// })
+// SHOW - users
+router.get("/:id", (req,res) =>{
+    res.send("GET for user id");
+})
 
-// // POST - users
-// router.post("/", (req,res) =>{
-//     res.send("POST for users");
-// })
+// POST - users
+router.post("/", (req,res) =>{
+    res.send("POST for users");
+})
 
-// // DELETE - users
-// router.delete("/:id", (req,res) =>{
-//     res.send("DELETE for user id");
-// })
+// DELETE - users
+router.delete("/:id", (req,res) =>{
+    res.send("DELETE for user id");
+})
 
-// module.exports = router;
+module.exports = router;
